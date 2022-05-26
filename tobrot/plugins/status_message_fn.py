11a9ba7a79e051b7cb525b19a59f73c556ac5651
WahyuMasamba <98741485+WahyuMasamba@users.noreply.github.com>
2022-05-26 14:01:45 +0700

    Update status_message_fn.py

--- tobrot/plugins/status_message_fn.py
+++ tobrot/plugins/status_message_fn.py
@@ -85,7 +85,6 @@ async def status_message_f(
 
                 percentage = int(file.progress_string(0).split('%')[0])
                 prog = "[{0}{1}]".format("".join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 5))]),"".join([UN_FINISHED_PROGRESS_STR for i in range(20 - math.floor(percentage / 5))]))
-                msg += f"<b>════════════════════════════════</b>\n"
                 msg += f"\n<b>{downloading_dir_name}</b>"
                 msg += f"\n<b>{prog}</b>"
                 msg += f"\n<b>Speed</b>: {file.download_speed_string()}"
@@ -93,6 +92,7 @@ async def status_message_f(
                 msg += f"\n<b>ETA:</b> {file.eta_string()}"
                 msg += f"\n{msgg}"
                 msg += f"\n<b>📛To Cancel:</b> <code>/cancel {file.gid}</code>"
+                msg += f"<b>▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬</b>\n"
                 msg += "\n"
 
         hr, mi, se = up_time(time.time() - BOT_START_TIME)
